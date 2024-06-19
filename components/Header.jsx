dc716e94569cd11830ad20b0fec2b3cd21cf0fc1
raviranjan mahto <rvrnjnmahto@gmail.com>
2024-06-19 11:26:33 +0530

    header added

---
+++ components/Header.jsx
@@ -0,0 +1,74 @@
+import Link from "next/link";
+import Image from "next/image";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import {
+  Briefcase,
+  HomeIcon,
+  MessagesSquare,
+  SearchIcon,
+  UsersIcon,
+} from "lucide-react";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <div className='flex items-center p-2 max-w-6xl mx-auto bottom-0 sticky top-0'>
+      {/* Icon */}
+      <Image
+        src='https://cdn-icons-png.flaticon.com/512/174/174857.png'
+        alt='logo'
+        width={40}
+        height={40}
+      />
+
+      {/* Search */}
+      <div className='flex-1'>
+        <form className='flex items-center space-x-1 bg-gray-100 p-2 rounded-md flex-1 mx-2 max-w-96'>
+          <SearchIcon className='h-4 text-gray-600' />
+          <input
+            type='text'
+            placeholder='Search'
+            className='bg-transparent flex-1 outline-none'
+          />
+        </form>
+      </div>
+
+      {/* Icon Right */}
+      <div className='flex items-center space-x-4 px-6'>
+        <Link href='/' className='icon'>
+          <HomeIcon className='h-5' />
+          <p>Home</p>
+        </Link>
+
+        <Link href='/' className='icon hidden md:flex'>
+          <UsersIcon className='h-5' />
+          <p>Network</p>
+        </Link>
+
+        <Link href='/' className='icon hidden md:flex'>
+          <Briefcase className='h-5' />
+          <p>Jobs</p>
+        </Link>
+
+        <Link href='/' className='icon'>
+          <MessagesSquare className='h-5' />
+          <p>Message</p>
+        </Link>
+
+        {/* User button if signed in */}
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+
+        {/* Sign In button if not signed in */}
+        <SignedOut>
+          <Button asChild variant='secondary'>
+            <SignInButton />
+          </Button>
+        </SignedOut>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
